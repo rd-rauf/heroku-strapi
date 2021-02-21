660c3cb4ac5d2239d8317b26aa3bc1a78678b212
Abdul Rauf <abdul.rauf@renovite.com>
2021-02-21 13:33:31 +0530

    Adding 'accounts' content-type

--- config/database.js
+++ config/database.js
@@ -4,6 +4,26 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "ec2-54-90-13-87.compute-1.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "d667cc1cir07dh"),
+        username: env("DATABASE_USERNAME", "ovgrxcwmezmkhe"),
+        password: env("DATABASE_PASSWORD", "c6c96d983527c56700a8c66de56ca35d3bcc4ae92cd207c1a17bbbe7a1b36f36"),
+        schema: "public",
+        ssl: { rejectUnauthorized: false }
+      },
+      options: {}
+    }
+  }
+});
+
+/* module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
         client: "sqlite",
         filename: env("DATABASE_FILENAME", ".tmp/data.db")
       },
@@ -13,3 +33,4 @@ module.exports = ({ env }) => ({
     }
   }
 });
+ */
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "ec2-54-90-13-87.compute-1.amazonaws.com"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        database: env("DATABASE_NAME", "d667cc1cir07dh"),
+        username: env("DATABASE_USERNAME", "ovgrxcwmezmkhe"),
+        password: env("DATABASE_PASSWORD", "c6c96d983527c56700a8c66de56ca35d3bcc4ae92cd207c1a17bbbe7a1b36f36"),
         schema: "public",
         ssl: { rejectUnauthorized: false }
       },
