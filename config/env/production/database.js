08c66d05748ce4d443b2d5dc86d9708fdaf6f2d4
Abdul Rauf <abdul.rauf@renovite.com>
2021-02-22 12:39:55 +0530

    env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "ec2-54-90-13-87.compute-1.amazonaws.com"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "d667cc1cir07dh"),
-        username: env("DATABASE_USERNAME", "ovgrxcwmezmkhe"),
-        password: env("DATABASE_PASSWORD", "c6c96d983527c56700a8c66de56ca35d3bcc4ae92cd207c1a17bbbe7a1b36f36"),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
         schema: "public",
         ssl: { rejectUnauthorized: false }
       },
